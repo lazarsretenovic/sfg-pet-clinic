736a62844f97c27a2371fb73888056eb7c01d89e
dev-ex000 <lazar.sretenovic@dev.telnet.rs>
2023-09-13 15:33:09 +0200

    Dependency Injection, Primary Beans and profile testings

--- sfg-di-primary-bean/src/main/java/guru/springframework/sfgdi/services/i18nEnglishGreetingService.java
+++ sfg-di-primary-bean/src/main/java/guru/springframework/sfgdi/services/i18nEnglishGreetingService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("EN")
+@Profile({"EN","default"})
 @Service("i18nService")
 public class i18nEnglishGreetingService implements GreetingService {
     @Override
